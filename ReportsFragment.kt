aa4ea7d3ac13c72293b5eba5fadc8cab9fd4c529
Eugene J. <41515840+s6raphin@users.noreply.github.com>
2024-09-13 22:33:06 +0200

    Create ReportsFragment.kt
    Fragments principaux pour générer des rapports.

---
+++ ReportsFragment.kt
@@ -0,0 +1,13 @@
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ReportsFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_reports, container, false)
+        // TODO: Implement reports generation and display
+        return view
+    }
+}
