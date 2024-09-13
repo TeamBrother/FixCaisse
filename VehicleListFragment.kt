74bc5cddb74db992c6fddd50996e789700a7d1b2
Eugene J. <41515840+s6raphin@users.noreply.github.com>
2024-09-13 22:29:56 +0200

    Create VehicleListFragment.kt
    fragments principaux pour afficher la liste des véhicules.

---
+++ VehicleListFragment.kt
@@ -0,0 +1,14 @@
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+
+class VehicleListFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_vehicle_list, container, false)
+        // TODO: Initialize RecyclerView and populate with vehicles
+        return view
+    }
+}
